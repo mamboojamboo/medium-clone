e0bd80b6c941576b88415a51317f6241ec1016cb
Mikhail <mvafonin@yandex.ru>
2020-01-09 15:32:01 +0300

    SignIn and SignUp

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import GlobalFeed from './pages/globalFeed/GlobalFeed';
+import Authentication from './pages/authentication/Authentication';
 import Article from './pages/article/Article';
 
 export default () => (
   <Switch>
     <Route path='/' component={GlobalFeed} exact />
+    <Route path='/login' component={Authentication} exact />
+    <Route path='/register' component={Authentication} exact />
     <Route path='/articles/:slug' component={Article}/>
   </Switch>
 );
