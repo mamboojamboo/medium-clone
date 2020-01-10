b35571ea48a83a86e75712bc446229bc2b1d11c5
Mikhail <mvafonin@yandex.ru>
2020-01-11 01:53:37 +0300

    Pagination finished

--- src/pages/globalFeed/GlobalFeed.jsx
+++ src/pages/globalFeed/GlobalFeed.jsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { stringify } from 'query-string';
 
 import useFetch from '../../hooks/useFetch';
 import Feed from '../../components/feed/Feed';
+import Pagination from '../../components/pagination/Pagination';
+import { getPaginator, limit } from '../../utils/utils';
 
-const GlobalFeed = () => {
-  const apiUrl = '/articles?limit=10&offset=0';
+const GlobalFeed = ({ location, match }) => {
+  const { offset, currentPage } = getPaginator(location.search);
+  const stringifiedParams = stringify({
+    limit,
+    offset
+  });
+  const apiUrl = `/articles?${stringifiedParams}`;
   const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+  const { url } = match;
 
   useEffect(() => {
     doFetch();
-  }, [doFetch]);
+  }, [doFetch, currentPage]);
 
   return (
     <div className='home-page'>
@@ -24,7 +33,16 @@ const GlobalFeed = () => {
           <div className='col-md-9'>
             {isLoading && <div>Loading...</div>}
             {error && <div>Some error happend</div>}
-            {!isLoading && response && <Feed articles={response.articles}/>}
+            {!isLoading && response && (
+              <Fragment>
+                <Feed articles={response.articles}/>
+                <Pagination
+                  total={response.articlesCount}
+                  limit={limit} url={url}
+                  currentPage={currentPage}
+                />
+              </Fragment>
+            )}
           </div>
           <div className='col-md-3'>
             Popular tags
