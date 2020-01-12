f7f07c918b84be6df0a636a692efd4c482a9c42d
Mikhail <mvafonin@yandex.ru>
2020-01-12 22:21:49 +0300

    AddToFav finished

--- src/components/addToFavorites/AddToFavorites.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AddToFavorites = ({ isFavorited, favoritesCount, articleSlug }) => (
-    <div>
-      AddToFavorites
-    </div>
-);
-
-export default AddToFavorites;
